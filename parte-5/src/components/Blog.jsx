dc6882661239e3543c662fa2c5403bfd34123e17
David Sliren <bluee.97.berry@gmail.com>
2026-04-22 19:16:51 -0500

    fix: Corregir error en el blog donde se deberia mostrar el titulo del blog y author, anteriormente solo mostaraba el titulo

--- parte-5/src/components/Blog.jsx
+++ parte-5/src/components/Blog.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Blog = ({ blog, handlerDelete, handlerUpdateLike }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   function handlerIsOpen() {
-    setIsOpen(!isOpen)
+    setIsOpen(!isOpen);
   }
 
   return (
     <div>
       <div className="title">
         <h2>{blog.title}</h2>
-        <button onClick={handlerIsOpen}>{isOpen ? 'Hidden' : 'View'}</button>
+        <p>{blog.author}</p>
+        <button onClick={handlerIsOpen}>{isOpen ? "Hidden" : "View"}</button>
         <button onClick={handlerDelete}>Delete</button>
       </div>
-      <div style={{ display: isOpen ? 'block' : 'none' }}>
-        <p>{blog.author}</p>
-
+      <div style={{ display: isOpen ? "block" : "none" }}>
         <img src={blog.url} alt={blog.title} width={300} />
         <br />
         <div>
@@ -26,7 +25,7 @@ const Blog = ({ blog, handlerDelete, handlerUpdateLike }) => {
       </div>
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
